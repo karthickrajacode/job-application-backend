6b960c6e00cee93e3dd024d409efd38e37a50260
karthick <kart87632@gmail.com>
2024-07-06 17:27:45 +0530

    Create a server.js file. import the packages & Mongo connection &  middleNames

---
+++ server.js
@@ -0,0 +1,33 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import morgan from "morgan";
+import bodyParser from "body-parser";
+
+import xss from "xss-clean";
+import mongoSanitize from "express-mongo-sanitize";
+import dbConnection from "./dbConfig/dbConnection.js";
+ 
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+//mongo Connection
+dbConnection();
+
+//middlenames
+app.use(cors());
+app.use(xss());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(mongoSanitize());
+app.use(express.json({limit:"10mb"}));
+app.use(express.urlencoded({extended:true}));
+
+app.use(morgan("dev"));
+
+app.listen(PORT,()=>{
+    console.log(`Server running on port : ${PORT}`);
+});
