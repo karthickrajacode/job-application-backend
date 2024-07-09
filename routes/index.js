9b7fb28547c67df3beaf2d0f3fd22e112f057c10
karthick <kart87632@gmail.com>
2024-07-09 18:47:26 +0530

    Update a index.js import userRoute.

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 
 import authRoute from "./authRouter.js";
+import userRoute from "./userRouter.js";
 
 const router = express.Router();
 
 const path = "/api-v1/";
 
 router.use(`${path}auth`, authRoute); //api-v1/auth/
+router.use(`${path}auth`, userRoute); 
 
 export default router;
