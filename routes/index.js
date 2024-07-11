d12e67d49ca01d324dab88dd1385d097ebcb2585
karthick <kart87632@gmail.com>
2024-07-11 16:26:33 -0700

    Update a index.js. import jobRoute.

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,15 @@ import express from "express";
 import authRoute from "./authRouter.js";
 import userRoute from "./userRouter.js";
 import companyRoute from "./companiesRouter.js"
+import jobRoute from "./jobRouter.js";
 
 const router = express.Router();
 
 const path = "/api-v1/";
 
 router.use(`${path}auth`, authRoute); //api-v1/auth/
-router.use(`${path}auth`, userRoute); 
-router.use(`${path}companies`,companyRoute)
+router.use(`${path}auth`, userRoute);
+router.use(`${path}companies`, companyRoute);
+router.use(`${path}jobs`, jobRoute)
 
 export default router;
