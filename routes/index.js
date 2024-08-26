156fd8c47958add2b128e1cc61ec782e95e9b4ec
karthick <kart87632@gmail.com>
2024-08-26 12:05:54 +0530

    Updated a index.js router.use(`${path}auth`, userRoute); to router.use(`${path}users`, userRoute);

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ const router = express.Router();
 const path = "/api-v1/";
 
 router.use(`${path}auth`, authRoute); //api-v1/auth/
-router.use(`${path}auth`, userRoute);
+router.use(`${path}users`, userRoute);
 router.use(`${path}companies`, companyRoute);
 router.use(`${path}jobs`, jobRoute)
 
