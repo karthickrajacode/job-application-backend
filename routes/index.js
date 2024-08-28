5078a95d450dcea7aeeb07eab005aca26d1b9599
karthick <kart87632@gmail.com>
2024-08-28 19:22:27 +0530

    router index.js is updated by including applicationRoute

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ import authRoute from "./authRouter.js";
 import userRoute from "./userRouter.js";
 import companyRoute from "./companiesRouter.js"
 import jobRoute from "./jobRouter.js";
+import applicationRoute from './applicationRouter.js'
 
 const router = express.Router();
 
@@ -13,5 +14,6 @@ router.use(`${path}auth`, authRoute); //api-v1/auth/
 router.use(`${path}users`, userRoute);
 router.use(`${path}companies`, companyRoute);
 router.use(`${path}jobs`, jobRoute)
+router.use(`${path}applications`, applicationRoute)
 
 export default router;
